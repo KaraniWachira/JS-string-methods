3511efd981ee3e81e4cceec6669f983a6988f8c7
Karani Wachira <wachirakeith@gmail.com>
2023-05-16 11:47:16 +0300

    use g for general calling of string element to search for

--- main.js
+++ main.js
@@ -2,17 +2,20 @@
 //lastIndexOf()
 
 // defining a string
-// let str = "Programming";
+//  let str = "Programming";
+//           //012345678910    
 
-//  defining a substring 'substr' that holds character 'g'
-// let substr = "g";
+// //  defining a substring 'substr' that holds character 'g'
+
+//  let substr = "g";
 
 // let fromIndex = 6;
 
-// passing second parameter 'fromIndex' in lastIndexOf()
-// let result = str.lastIndexOf(substr, fromIndex);
+// // passing second parameter 'fromIndex' in lastIndexOf()
 
-// console.log(result);
+//  let result = str.lastIndexOf(substr, fromIndex);
+
+//  console.log(result);
 
 
 
@@ -25,13 +28,23 @@
 // console.log(match);
 
 
-// secondly: can be used to find the index of the first occurrence of a match in a string
+// secondly:
+//can be used to find the index of the first occurrence of a match in a string
+
 // let string = "The quick brown fox jumps over the lazy dog."; 
 
 // let match = string.match(/The/);
 
 // console.log(match.index);
 
+
+//Example: 
+let stl = "Hello World";
+stl.match(/l/);
+
+console.log(stl.match(/l/g));
+
+//g used to find all the matches in a string
 
 
 
